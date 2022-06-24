84ab1e9eb94fbee56c0fb02805d24a40a1966a95
Eabulkaliq <sam.abulkaliq@gmail.com>
2022-06-24 15:22:18 -0400

    first commit midterm

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  // "URI": "mongodb://localhost/books229"
+  URI: "mongodb+srv://eabdulklaiq:Sam_179800@cluster0.6ea0b.mongodb.net/?retryWrites=true&w=majority",
+};
